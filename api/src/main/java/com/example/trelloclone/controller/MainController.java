787a1ec1dc7b6f293dcfc5bf4b15c7abbf34f67f
moritasoshi <soshi.morita@rakus-partners.co.jp>
2021-04-22 10:09:44 +0900

    Create a sign up func

--- api/src/main/java/com/example/trelloclone/controller/MainController.java
+++ api/src/main/java/com/example/trelloclone/controller/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.example.trelloclone.security.SecurityConstants.SIGN_UP_URL;
+
 @RestController
 public class MainController {
 
@@ -15,7 +17,7 @@ public class MainController {
         this.userService = userService;
     }
 
-    @PostMapping("/auth/user/sign-up")
+    @PostMapping(SIGN_UP_URL)
     public void createUser(@RequestBody User user){
         userService.createUser(user);
     }
--- api/src/main/java/com/example/trelloclone/security/SecurityConstants.java
+++ api/src/main/java/com/example/trelloclone/security/SecurityConstants.java
@@ -5,5 +5,8 @@ public class SecurityConstants {
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/api/users/sign-up";
+    public static final String SIGN_UP_URL = "/auth/sign-up";
+    public static final String LOGIN_URL = "/auth/login";
+    public static final String USERNAME_PARAMETER = "email";
+    public static final String PASSWORD_PARAMETER = "password";
 }
