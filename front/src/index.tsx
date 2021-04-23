37363efaa4c7f1c44023d146a292f2e6affcff87
moritasoshi <soshi.morita@rakus-partners.co.jp>
2021-04-23 22:11:54 +0900

    Header作成とLoginフォームの土台作成

---
+++ front/src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Login from './pages/LoginPage';
+import Header from './templates/Header';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Header title={ 'Trello Clone' }/>
+    <Switch>
+      <Route exact path="/">
+        <Login />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
---
+++ front/src/templates/Header.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbarTitle: {
+    flex: 1,
+  },
+  toolbarSecondary: {
+    justifyContent: 'space-between',
+    overflowX: 'auto',
+  },
+  toolbarLink: {
+    padding: theme.spacing(1),
+    flexShrink: 0,
+  },
+}));
+
+type Props = {
+  title: string
+}
+
+export default function Header(props: Props) {
+  const classes = useStyles();
+  const { title } = props;
+
+  return (
+    <React.Fragment>
+      <Toolbar className={classes.toolbar}>
+        <Button size="small">Subscribe</Button>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          className={classes.toolbarTitle}
+        >
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            {title}
+          </Link>
+        </Typography>
+        <Button variant="outlined" size="small">
+          <Link to="/login" style={{ textDecoration: 'none' }}>
+            Sign in
+          </Link>
+        </Button>
+      </Toolbar>
+    </React.Fragment>
+  );
+}
